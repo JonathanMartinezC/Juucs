fe2813d95de6ab2a065d3971b767faf7bb14a52a
Jonathan Jeremy Martinez Carranza <99061848+JonathanMartinezC@users.noreply.github.com>
2022-04-05 14:50:07 -0500

    Update: register.php

--- Proyecto/logica/register.php
+++ Proyecto/logica/register.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 include_once 'conexion.php';
-$newName = $_POST['newName'];
 $newUser = $_POST['newUser'];
 $newEmail = $_POST['newEmail'];
 $newPassword = $_POST['newPassword'];
@@ -9,14 +8,14 @@ $newPassword = $_POST['newPassword'];
 $queryStructureLookForUser = 
 "SELECT correo
  FROM usuario
- WHERE correo = '$newEmail'";
+ WHERE correo = '$newEmail' or nom_usuario = '$newUser' ";
 
 $checkIfUserExists = mysqli_query($conexion, $queryStructureLookForUser);
 
 if (mysqli_num_rows($checkIfUserExists) == 0) {
     $queryStructureNewUser = 
-    "INSERT INTO usuario (nombre, nom_usuario, correo, contrasenia)
-    VALUES ('$newName', '$newUser', '$newEmail', '$newPassword')";
+    "INSERT INTO usuario (nom_usuario, correo, contrasenia)
+    VALUES ('$newUser', '$newEmail', '$newPassword')";
 
     $insertNewUser = mysqli_query($conexion, $queryStructureNewUser);
 
@@ -30,10 +29,9 @@ if (mysqli_num_rows($checkIfUserExists) == 0) {
         echo "<a href='../index.html'> Volver </a>";
     }
 } else {
-    echo "El correo electronico que insertaste ya esta en uso.";
+    echo "El nombre de usuario o correo electronico que insertaste ya esta en uso.";
     echo "Intente nuevamente";
-    echo "<a href='../index.html'> Volver </a>";
+    echo "<a href='../registerSite.php'> Volver </a>";
 }
-
 
 ?>
